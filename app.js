7d4baf81a8ba8e8c18586a9d8867aca71d55ea2f
AAflip <jedidjm2008@gmail.com>
2025-04-07 14:39:03 -0700

    some of everything

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 //variables
+let storyObj = {
+    
+}
 
 //initial function, all functions that should be run on start go in here
 function init(){
-
+    movePage('mainView');
+    summonDialog('on');
 }
 
 //movePage function, shifts display of the sections, not the content
@@ -12,4 +16,26 @@ function movePage(to){
         section.style.display = 'none';
     }
     document.getElementById(to).style.display = 'flex';
+}
+
+//summonDialog function, makes the dialog box visible/not visible
+function summonDialog(state){
+    let box = document.getElementById('dialogBox');
+    if(state == 'on'){
+        box.style.display = 'flex';
+    }else{
+        box.style.display = 'none';
+    }
+}
+
+//playSound function, plays inputted sound
+function playSound(name, volume=1){
+    let sound = new Audio("sounds/"+name+".mp3")
+    sound.volume = (volume <= 1) ? volume : 1;
+    sound.play();
+}
+
+// updateDialog function, updates the dialog in the dialog box
+function updateDialog(){
+
 }
